0501ba22a8d8d93859139a4c34cdb07504346f69
Blake Molina <blakeamolina@gmail.com>
2018-04-30 19:22:51 -0700

    Lay groundwork for main algorithm

--- graph.hpp
+++ graph.hpp
@@ -4,47 +4,47 @@
 		- map each key of where vertex is  
 */
 #include <vector>
-
 #include <iostream>
 #include <iomanip>
 
+template <typename Vertex_Type>
+struct Vertex{
+	Vertex_Type value;
+	int index; 	//index within adj matrix and vertices set
+};
 
 template <typename Vertex_Type, typename Edge_Type>
 class Graph{
 public:
-	struct Vertex{
-		Vertex_Type value;
-		int index; 	//index within adj matrix and vertices set
-	};
 
 	Graph();
 	~Graph();
 
-	void addVertex(Vertex_Type a); 
-	void removeVertex(Vertex_Type a); 
-
-	
-	void addEdge(Vertex_Type a, Vertex_Type b, Edge_Type value); 
+	void addVertex(Vertex_Type a);
+	void removeVertex(Vertex_Type a);
+	void addEdge(Vertex_Type a, Vertex_Type b, Edge_Type value);
 
 	Edge_Type at(Vertex_Type a, Vertex_Type b);
-	//gt array of adjacent vertices 
-	std::vector<Vertex_Type> adjacent(Vertex_Type a);
-	
+	std::vector<Vertex_Type> adjacent(Vertex_Type a); //gt array of adjacent vertices
+
 	void print();
-	
+	bool isEmpty() const;
+	size_t density() const;
+	size_t size() const;
+
 private:
 
 	int vertexIndex(Vertex_Type a);
-	//all vectors
-	std::vector<Vertex> m_vertices;
-	//edges
-	std::vector<std::vector<Edge_Type> > m_adj_matrix;
+	std::vector<Vertex> m_vertices; //all vectors
+	std::vector<std::vector<Edge_Type>> m_adj_matrix; //edges
+	size_t num_edges;
 };
 
 template <typename Vertex_Type, typename Edge_Type>
 Graph<Vertex_Type, Edge_Type>::Graph()
-
-{}
+{
+	num_edges = 0;
+}
 
 template <typename Vertex_Type, typename Edge_Type>
 Graph<Vertex_Type, Edge_Type>::~Graph(){}
@@ -65,7 +65,7 @@ void Graph<Vertex_Type, Edge_Type>::print(){
 			std::cout << std::setw(5) << *it;
 		std::cout << '\n';
 	}
-	
+
 }
 
 
@@ -76,37 +76,37 @@ void Graph<Vertex_Type, Edge_Type>::addVertex(Vertex_Type a){
 	if(i == -1){ //does not exist so add
 		i = m_vertices.size();
 		Vertex v = {a, i};
-		m_vertices.push_back(v); 
+		m_vertices.push_back(v);
 		m_adj_matrix.push_back(std::vector<Edge_Type>());
-		for(typename std::vector<std::vector<Edge_Type> >::iterator it = m_adj_matrix.begin(); 
+		for(typename std::vector<std::vector<Edge_Type> >::iterator it = m_adj_matrix.begin();
 			it!= m_adj_matrix.end(); it++){
 			it->resize(i+1);
 		}
-		
+
 	}
-} 
+}
 
 template <typename Vertex_Type, typename Edge_Type>
 void Graph<Vertex_Type, Edge_Type>::removeVertex(Vertex_Type a){
 	int i = vertexIndex(a);
 	if(i != -1){ // exists so del
 		m_vertices.erase(m_vertices.begin()+i);
-		 
-		for(typename std::vector<std::vector<Edge_Type> >::iterator it = m_adj_matrix.begin(); 
-							it!= m_adj_matrix.end(); it++){
+
+		for(typename std::vector<std::vector<Edge_Type> >::iterator it = m_adj_matrix.begin();
+			it!= m_adj_matrix.end(); it++){
 			it->erase(it->begin()+i);
 		}
 		//update all indices starting at i
-		for(typename std::vector<Vertex>::iterator it = m_vertices.begin()+i; it != m_vertices.end(); it++) 
+		for(typename std::vector<Vertex>::iterator it = m_vertices.begin()+i; it != m_vertices.end(); it++)
 			it->index--;
 	}
 }
 
 template <typename Vertex_Type, typename Edge_Type>
 int Graph<Vertex_Type, Edge_Type>::vertexIndex(Vertex_Type a){
-	for(typename std::vector<Vertex>::iterator it = m_vertices.begin(); it != m_vertices.end(); it++) 
-			if(it->value == a)
-				return it->index;
+	for(typename std::vector<Vertex>::iterator it = m_vertices.begin(); it != m_vertices.end(); it++)
+		if(it->value == a)
+			return it->index;
 	return -1;
 }
 
@@ -117,9 +117,10 @@ void Graph<Vertex_Type, Edge_Type>::addEdge(Vertex_Type a, Vertex_Type b, Edge_T
 		int j = vertexIndex(b);
 		if(j != -1){ // exists so del
 			m_adj_matrix[i][j] = value;
+			num_edges++;
 		}
 	}
-} 
+}
 template <typename Vertex_Type, typename Edge_Type>
 Edge_Type Graph<Vertex_Type, Edge_Type>::at(Vertex_Type a, Vertex_Type b){
 	int i = vertexIndex(a);
@@ -132,7 +133,7 @@ Edge_Type Graph<Vertex_Type, Edge_Type>::at(Vertex_Type a, Vertex_Type b){
 	return Edge_Type();
 }
 
-//gt array of adjacent vertices 
+//gt array of adjacent vertices
 template <typename Vertex_Type, typename Edge_Type>
 std::vector<Vertex_Type> Graph<Vertex_Type, Edge_Type>::adjacent(Vertex_Type a){
 	std::vector<Vertex_Type> adj;
@@ -147,4 +148,36 @@ std::vector<Vertex_Type> Graph<Vertex_Type, Edge_Type>::adjacent(Vertex_Type a){
 	}
 	return adj;
 }
+/**
+ * Computes and returns the density of the graph. The density function is
+ * simply the number of edges divided by the number of vertices, or D = E/V.
+ *
+ * @return The density of the graph.
+ */
+template <typename Vertex_Type, typename Edge_Type>
+size_t Graph<Vertex_Type, Edge_Type>::density() const {
+	return isEmpty() ? 0 : (num_edges / m_vertices.size());
+}
+
+/**
+ * Returns true if this graph contains no elements.
+ *
+ * @return True if the graph is empty, false if not.
+ */
+template <typename Vertex_Type, typename Edge_Type>
+bool Graph<Vertex_Type, Edge_Type>::isEmpty() const {
+	return m_vertices.empty();
+}
+
+/**
+ * Returns the size of the graph (number of vertices).
+ *
+ * @return The number of vertices.
+ */
+template <typename Vertex_Type, typename Edge_Type>
+size_t Graph<Vertex_Type, Edge_Type>::size() const {
+	return m_vertices.size();
+}
+
+
 	
--- main.cpp
+++ main.cpp
@@ -1,27 +1,62 @@
 #include "graph.hpp"
+#include <mpi.h>
 
-//c++11
-int main(){
+/**
+ * Returns a unique subset of vertices from a non-empty graph object, based on the world_size
+ * and world_rank parameters.
+ *
+ * @param graph A non-empty graph object.
+ * @param rank The world rank of process.
+ * @return A unique subset of vertices from the graph.
+ */
+std::vector<Vertex> PartitionGraph(Graph<int,int> &graph, int world_size, int world_rank){
+    assert(!graph.isEmpty());
+    assert(world_rank >= 0);
 
-	Graph<int, int> g;
-	for(int i = 0; i < 10; i++){
-		g.addVertex(i );
-		for(int j = 0; j < 10; j++){
-			g.addEdge(i,j, i);
-			g.addEdge(j,i, j);
-		}
-	}		
-	g.print();
-	std::cout << '\n';
-	g.removeVertex(3);
-	g.removeVertex(0);
-	g.removeVertex(9);
-	g.removeVertex(7);
-	g.addVertex(3);
-	g.addEdge(3,5, 10);
-	g.print();
-	std::cout << '\n';
-	std::vector<int> adj = g.adjacent(2);
-	for(typename std::vector<int>::iterator adjIt = adj.begin(); adjIt != adj.end(); adjIt++)
-		std::cout << " " << *adjIt;
+    size_t num_vertices = graph.size() / world_size;
+    std::vector<Vertex> result;
+    result.reserve(num_vertices);
+
+    // Get vertices from the graph. Offsetting by the world_rank retrieves distinct vertices.
+    for (int i = 0; i < num_vertices; i++){
+        // TODO: Get vertices from the graph
+    }
+
+    return result;
+};
+
+int main(int argc, char** argv){
+
+    Graph<int, int> original;
+    Graph<int, int> result;
+    int world_rank;
+    int world_size;
+    size_t density;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Make sure there aren't more processes than vertices in the graph.
+    assert(original.size() > world_size);
+
+    density = original.density();
+
+    if (world_rank != 0){
+        auto sub_graph = PartitionGraph(original, world_size, world_rank);
+        for (auto const& vertex : sub_graph){
+            // TODO: Compute degree of each vertex and if it's <= 2(1+e) * p(S), return the vertices to remove to process 0.
+        }
+
+         // TODO: Send information back to process 0 so that it can reduce the map
+
+    } else{
+        // TODO: Collect information from child processes
+        // TODO: Reduce graph, calculate density (P) and store new graph in 'result' if P > the before P value.
+        // TODO: Continue looping until the original graph is empty.
+    }
+
+    result.print();
+
+    MPI_Finalize();
 }
