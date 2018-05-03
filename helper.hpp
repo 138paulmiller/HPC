03c94d08c556d0bb24885d6b76c39a360a47f4d6
Blake Molina <blakeamolina@gmail.com>
2018-05-02 21:49:41 -0700

    Improve calculating send counts and stride values

--- helper.hpp
+++ helper.hpp
@@ -6,34 +6,28 @@
 /**
  * This function calculates two things:
  *      1). It computes the number of vertices that each process will work with (N).
- *      2). It computes the starting index that each process will receive N vertices from (the offset).
+ *      2). It computes the starting index that each process will receive N vertices from (the stride value).
  *
- * The results of both of these are stored in @param send_counts and @param offset.
+ * The results of both of these are stored in @param sendcounts and @param strides.
  *
- * @param graph A graph object.
+ * @param num_vertices The number of vertices in the graph.
  * @param world_size The number of processes.
- * @param send_counts A vector containing the number of vertices to delegate to each process.
- * @param offset A vector containing the starting index for each process to start getting vertices from.
+ * @param sendcounts A vector containing the number of vertices to delegate to each process.
+ * @param strides A vector containing the starting index for each process to start getting vertices from.
  *
  */
-void computeSendCounts(Graph<int, int> &graph, int world_size, std::vector<int> &send_counts, std::vector<int>& offset){
+void computeSendAndStrideCounts(int num_vertices, int world_size, std::vector<int> &sendcounts, std::vector<int>& strides){
 
-    // If there are less vertices than processes, process 0 will be delegated with all of the vertices.
-    if(graph.size() / world_size < 1){
-        send_counts.resize(1, graph.size());
-        offset.resize(1,0);
-    }
-    else {
-        int v_per_rank = (int)ceil(graph.size() / world_size);
-        send_counts.resize(world_size, v_per_rank);
-        offset.resize(world_size);
-
-        send_counts[world_size - 1] = graph.size() - (world_size - 1) * v_per_rank;
-        int temp = 0;
-        for (int i = 0; i < world_size; ++i) {
-            offset[i] = temp;
-            temp += send_counts[i];
+    int sum = 0;
+    int remainder = (num_vertices) % world_size;
+    for (int i = 0; i < world_size; i++) {
+        sendcounts[i] = (num_vertices) / world_size;
+        if (remainder > 0) {
+            sendcounts[i]++;
+            remainder--;
         }
+        strides[i] = sum;
+        sum += sendcounts[i];
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -2,22 +2,20 @@
 #include <cstddef>
 #include <mpi.h>
 
+int main(int argc, char *argv[]) {
 
-
-int main(int argc, char** argv) {
-
-    Graph<int, int> original;                   // The original graph.
-    Graph<int, int> result;                     // The result graph.
-    int world_rank;                             // The unique ID of the process.
-    int world_size;                             // The number of processes.
-    size_t density;                             // The graph density.
-    std::vector<int> send_counts;               // Contains the number of vertices for each process to work with.
-    std::vector<int> offsets;                    // Contains the starting index of the vertex vector for each process.
-    std::vector<Vertex<int>> receive_buffer;    // Buffer containing the subset of vertices from the graph to work with.
-    const int N_VERT = 3;                       // The number of vertices to create.
+    Graph<int, int> original;                 // The original graph.
+    Graph<int, int> result;                   // The result graph.
+    int world_rank;                           // The unique ID of the process.
+    int world_size;                           // The number of processes.
+    size_t density;                           // The graph density.
+    std::vector<int> sendcounts;              // Contains the number of vertices for each process to work with.
+    std::vector<int> strides;                 // Contains the starting index of the vertex vector for each process.
+    std::vector<Vertex<int>> receive_buffer;  // Buffer containing the subset of vertices from the graph to work with.
+    const int N_VERT = 15;                     // The number of vertices to create.
 
     // MPI Initialization
-    MPI_Init(nullptr, nullptr);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -27,55 +25,51 @@ int main(int argc, char** argv) {
             original.addVertex(i);
     }
 
+    receive_buffer.resize(world_size);
+    sendcounts.resize(world_size);
+    strides.resize(world_size);
 
     // Create a new MPI data type for our Vertex struct.
     const int nitems = 3;
     int blocklengths[3] = {1, 1, 1};
     MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
     MPI_Datatype mpi_vertex_type;
-    MPI_Aint struct_offsets[3];
-    struct_offsets[0] = offsetof(Vertex<int>, value);
-    struct_offsets[1] = offsetof(Vertex<int>, index);
-    struct_offsets[2] = offsetof(Vertex<int>, degree);
-    MPI_Type_create_struct(nitems, blocklengths, struct_offsets, types, &mpi_vertex_type);
+    MPI_Aint offsets[3];
+    offsets[0] = offsetof(Vertex<int>, value);
+    offsets[1] = offsetof(Vertex<int>, index);
+    offsets[2] = offsetof(Vertex<int>, degree);
+    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_vertex_type);
     MPI_Type_commit(&mpi_vertex_type);
 
     // Compute initial density of the graph
     density = original.density();
 
-    if (world_rank == 0) {
-        computeSendCounts(original, world_size, send_counts, offsets);
+    // Compute the send counts and stride values.
+    computeSendAndStrideCounts(N_VERT, world_size, sendcounts, strides);
 
-        std::cout << "\nSENDS:\n";
-        for (int i = 0; i < send_counts.size(); i++) {
-            std::cout << send_counts[i] << " ";
+    // Print send counts and stride values. *** DEBUG ***
+    if (world_rank == 0){
+        for (int i = 0; i < world_size; i++) {
+            printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, strides[i]);
         }
-        std::cout << "\nDISP:\n";
-        for (int i = 0; i < offsets.size(); i++) {
-            std::cout << offsets[i] << " ";
-        }
-        std::cout << "\n";
+        std::cout << "\n\n";
     }
-    receive_buffer.resize(world_size);
-    if(send_counts.size() > 1) {
 
-        MPI_Scatterv(original.toArray(), &send_counts[0], &offsets[0], mpi_vertex_type,
+    // Divide the data among the processes as described by sendcounts and strides.
+    if (sendcounts[world_rank] != 0) {
+        MPI_Scatterv(original.toArray(), &sendcounts[0], &strides[0], mpi_vertex_type,
                      &receive_buffer[0], receive_buffer.size(), mpi_vertex_type,
                      0, MPI_COMM_WORLD);
+    }
+    // Processes that have no work to do will wait until the scatter completes.
+    MPI_Barrier(MPI_COMM_WORLD);
 
-        MPI_Barrier(MPI_COMM_WORLD);
-        for (int i = 0; i < receive_buffer.size(); i++){
-            std::cout << "\nP" << world_rank << ": " << receive_buffer[i].value;
-        }
+
+    // Print what each process received. *** DEBUG ***
+    for (int i = 0; i < sendcounts[world_rank]; i++) {
+        printf("P%d: %d\n", world_rank, receive_buffer[i].value);
     }
-        // only one, root will handle
-    else{
-        if(world_rank == 0){
-            for (int i = 0; i < original.size(); i++){
-                std::cout << "\nP" << world_rank << ": " << original.toArray()[i].value;
-            }
-        }
-    }
+
 
     MPI_Finalize();
 }
