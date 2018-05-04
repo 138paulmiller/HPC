bba7127b4c43a24440f3e05e11efb7cc1390bf7a
Blake Molina <blakeamolina@gmail.com>
2018-05-03 20:38:55 -0700

    Pushing untested changes

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
     std::vector<Vertex<int>> receive_buffer;  // Buffer containing the subset of vertices from the graph to work with.
     const int N_VERT = 11;                    // The number of vertices to create.
     const int EPSILON = 1;                    // Heuristic value for removing vertices.
+    int LOOP = 1;                             // LCV; set to 0 to exit while loop.
 
     // MPI Initialization
     MPI_Init(&argc, &argv);
@@ -33,47 +34,72 @@ int main(int argc, char *argv[]) {
     MPI_Datatype mpi_vertex_type;
     CreateVertexTypeMPI(mpi_vertex_type);
 
-    ComputeSendAndStrideCounts(sendcounts, strides, world_size, N_VERT);
+    // Will continue looping until the original graph is empty.
+    while (LOOP){
 
-    // *** DEBUG ***
-    if (world_rank == 0){
-        for (int i = 0; i < world_size; i++) {
-            printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, strides[i]);
+        ComputeSendAndStrideCounts(sendcounts, strides, world_size, original.size());
+
+        // *** DEBUG ***
+        if (world_rank == 0){
+            for (int i = 0; i < world_size; i++) { printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, strides[i]); }
+            std::cout << "\n\n";
         }
-        std::cout << "\n\n";
+
+        // Divide the data among the processes as described by sendcounts and strides.
+        if (sendcounts[world_rank] != 0) {
+            MPI_Scatterv(original.toArray(), &sendcounts[0], &strides[0], mpi_vertex_type,
+                         &receive_buffer[0], receive_buffer.size(), mpi_vertex_type,
+                         0, MPI_COMM_WORLD);
+        }
+        // Processes that have no work to do will wait until the scatter completes.
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // Broadcast the density to all processes.
+        MPI_Bcast(&density, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // *** DEBUG ***
+        for (int i = 0; i < sendcounts[world_rank]; i++) { printf("P%d: %d\n", world_rank, receive_buffer[i].value); }
+
+        auto verticesToRemove = ComputeVerticesToRemove(receive_buffer, EPSILON, density);
+
+        // *** DEBUG ***
+        for (int i = 0; i < verticesToRemove.size(); i++) { printf("\nP%d: %d", world_rank, verticesToRemove[i].value); }
+
+
+        // We are going to reuse these buffers, so make sure they are clear. (might just be able to directly write over these?)
+        sendcounts.clear();
+        strides.clear();
+
+        // Recompute the send and stride counts for when we gather all of the vertices to remove to the root process.
+        ComputeSendAndStrideCounts(sendcounts, strides, world_size, verticesToRemove.size());
+
+        MPI_Gatherv(&verticesToRemove[0], verticesToRemove.size(), mpi_vertex_type, &receive_buffer[0],
+                    &sendcounts[0], &strides[0], mpi_vertex_type, 0, MPI_COMM_WORLD);
+
+        // Remove vertices from graph update the graph density.
+        if (world_rank == 0){
+            for (auto & v : receive_buffer){
+                original.removeVertex(v.value);
+            }
+            if (original.density() > density){
+                density = original.density();
+                result = original;
+            }
+            // Update LCV in all processes.
+            if (original.isEmpty()){
+                LOOP = 0;
+                MPI_Bcast(&LOOP, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            }
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
     }
 
-    // Divide the data among the processes as described by sendcounts and strides.
-    if (sendcounts[world_rank] != 0) {
-        MPI_Scatterv(original.toArray(), &sendcounts[0], &strides[0], mpi_vertex_type,
-                     &receive_buffer[0], receive_buffer.size(), mpi_vertex_type,
-                     0, MPI_COMM_WORLD);
+    // Print results.
+    if (world_rank == 0){
+        result.print();
+        std::cout << "\nDensity: " << result.density();
     }
-    // Processes that have no work to do will wait until the scatter completes.
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // Broadcast the density to all processes.
-    MPI_Bcast(&density, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // *** DEBUG ***
-    for (int i = 0; i < sendcounts[world_rank]; i++) { printf("P%d: %d\n", world_rank, receive_buffer[i].value); }
-
-    auto verticesToRemove = ComputeVerticesToRemove(receive_buffer, EPSILON, density);
-
-    // *** DEBUG ***
-    for (int i = 0; i < verticesToRemove.size(); i++) { printf("\nP%d: %d", world_rank, verticesToRemove[i].value); }
-
-
-    // We are going to reuse these buffers, so make sure they are clear. (might just be able to directly write over these?)
-    sendcounts.clear();
-    strides.clear();
-
-    // Recompute the send and stride counts for when we gather all of the vertices to remove to the root process.
-    ComputeSendAndStrideCounts(sendcounts, strides, world_size, verticesToRemove.size());
-
-    MPI_Gatherv(&verticesToRemove[0], verticesToRemove.size(), mpi_vertex_type, &receive_buffer[0],
-                &sendcounts[0], &strides[0], mpi_vertex_type, 0, MPI_COMM_WORLD);
-    
 
     MPI_Finalize();
 }
